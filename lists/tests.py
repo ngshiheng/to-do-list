2c0a797a00dc4c91511a27cb5a75eeeb28f73baa
Ng, Jerry Shi Heng <jerry.shi.heng.ng@intel.com>
2019-07-25 00:14:53 +0800

    Commit before refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -11,10 +11,15 @@ class HomePageTest(TestCase):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
 
-	def test_home_page_returns_correct_html(self):
-		response = self.client.get('/')
-		html = response.content.decode('utf8')
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>', html)
-		self.assertTrue(html.endswith('</html>'))
+	# def test_home_page_returns_correct_html(self):
+	# 	response = self.client.get('/')
+	# 	html = response.content.decode('utf8')
+	# 	self.assertTrue(html.startswith('<html>'))
+	# 	self.assertIn('<title>To-Do lists</title>', html)
+	# 	self.assertTrue(html.endswith('</html>'))
+	# 	self.assertTemplateUsed(response, 'home.html')
+	
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
