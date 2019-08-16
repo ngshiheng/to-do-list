69f4435f625a890fdedfde6ede7f4790a98c19bb
ngshiheng <ngshiheng@gmail.com>
2019-08-16 20:08:34 +0800

    Implement duplicate item validation at model layer

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -18,9 +18,9 @@ class LayoutAndStylingTest(FunctionalTest):
         )
 
         # She starts a new list and sees the input is nicely centered there too
-        inputbox.send_keys('testing')
+        inputbox.send_keys('Layout testing')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
+        self.wait_for_row_in_list_table('1: Layout testing')
         inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
--- lists/models.py
+++ lists/models.py
@@ -9,5 +9,12 @@ class List(models.Model):
 
 
 class Item(models.Model):
-    text = models.TextField(default='')
+    text = models.TextField(default='', unique=False)
     list = models.ForeignKey(List, default=None)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
