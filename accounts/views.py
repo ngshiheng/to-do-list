fd6638f374c5c901cebc252c53267522bfbda1df
ngshiheng <ngshiheng@gmail.com>
2019-08-18 23:37:12 +0800

    Added User login feature via email + unit tests

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+
+
+def send_login_email(request):
+    email = request.POST['email']
+    send_mail(
+        'Your login link for Superlists',
+        'body text tbc',
+        'noreply@superlists',
+        [email],
+    )
+    return redirect('/')
