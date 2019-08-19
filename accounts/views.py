17451760851d33b6a4c39c457dc8c536e6bc3b50
ngshiheng <ngshiheng@gmail.com>
2019-08-19 23:30:34 +0800

    q qasdasd

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 from django.core.mail import send_mail
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from accounts.models import Token
 
 
 def send_login_email(request):
     email = request.POST['email']
-    print(type(send_mail))
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
+
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        message_body,
         'noreply@superlists',
-        [email],
+        [email]
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
     )
     return redirect('/')
+
+
+def login(request):
+    return redirect('/')
+#
+#
+# def logout(request):
+#     auth_logout(request)
+#     return redirect('/')
