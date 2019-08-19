6d416bb27e5ba0bfbde604dd1f5fde487874e6ef
ngshiheng <ngshiheng@gmail.com>
2019-08-19 23:31:16 +0800

    Revert "q"
    This reverts commit 17451760851d33b6a4c39c457dc8c536e6bc3b50.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,4 @@ from accounts import views
 
 urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
-    url(r'^login$', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,37 +1,14 @@
 from django.shortcuts import render, redirect
-from django.core.urlresolvers import reverse
 from django.core.mail import send_mail
-from django.contrib import messages
-from django.contrib.auth import authenticate
-from django.contrib.auth import login as auth_login, logout as auth_logout
-from accounts.models import Token
 
 
 def send_login_email(request):
     email = request.POST['email']
-    token = Token.objects.create(email=email)
-    url = request.build_absolute_uri(
-        reverse('login') + '?token=' + str(token.uid)
-    )
-    message_body = f'Use this link to log in:\n\n{url}'
-
+    print(type(send_mail))
     send_mail(
         'Your login link for Superlists',
-        message_body,
+        'body text tbc',
         'noreply@superlists',
-        [email]
-    )
-    messages.success(
-        request,
-        "Check your email, we've sent you a link you can use to log in."
+        [email],
     )
     return redirect('/')
-
-
-def login(request):
-    return redirect('/')
-#
-#
-# def logout(request):
-#     auth_logout(request)
-#     return redirect('/')
