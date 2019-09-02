eec9acf6a88a0508f1a4bfeb5112e26edfaa05f3
ngshiheng <ngshiheng@gmail.com>
2019-09-02 16:48:43 +0800

    Add virutalenv in gitignore & updated hostname param

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -3,24 +3,23 @@ from fabric.context_managers import settings, shell_env
 
 
 def _get_manage_dot_py(host):
-    return f'/home/ubuntu/to-do-list/venv-to-do-list/bin/python ~/to-do-list/manage.py'
+    return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/manage.py'
 
 
 def reset_database(host):
-    host = 'ubuntu'
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'ubuntu@ip-172-31-18-227'):
+    with settings(host_string=f'ubuntu@{host}'):
         run(f'{manage_dot_py} flush --noinput')
 
 
 def _get_server_env_vars(host):
-    env_lines = run(f'cat ~/to-do-list/.env').splitlines()
+    env_lines = run(f'cat ~/sites/{host}/.env').splitlines()
     return dict(l.split('=') for l in env_lines if l)
 
 
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'ubuntu@ip-172-31-18-227'):
+    with settings(host_string=f'ubuntu@{host}'):
         env_vars = _get_server_env_vars(host)
         with shell_env(**env_vars):
             session_key = run(f'{manage_dot_py} create_session {email}')
