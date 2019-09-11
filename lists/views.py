89cb6c4e6d6a35b3bc74c83a2463fcb9c1441e56
ngshiheng <ngshiheng@gmail.com>
2019-09-11 08:10:12 +0800

    Rewriting tests for view to be full isolated, passes all UT

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect
 from lists.models import List
-from lists.forms import ItemForm, ExistingListItemForm
+from lists.forms import ItemForm, ExistingListItemForm, NewListForm
 
 User = get_user_model()
 
@@ -31,6 +31,14 @@ def new_list(request):
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
+
+
+def new_list2(request):
+    form = NewListForm(data=request.POST)
+    if form.is_valid():
+        list_ = form.save(owner=request.user)
+        return redirect(list_)
+    return render(request, 'home.html', {'form': form})
 
 
 def my_lists(request, email):
