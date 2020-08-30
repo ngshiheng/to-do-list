62a54388ded9e91569fcb263b36603dec2d376cd
Jerry Ng <jerry@botmd.io>
2020-08-30 10:45:04 +0800

    Fix authentication

--- accounts/views.py
+++ accounts/views.py
@@ -27,8 +27,7 @@ def send_login_email(request):
 
 
 def login(request):
-    user = auth.authenticate(uid=request.GET.get('token'))
+    user = auth.authenticate(request.GET.get('token'))
     if user:
         auth.login(request, user)
     return redirect('/')
-
